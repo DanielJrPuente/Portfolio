029f977d58df5886188b1cb8a48196e3f8578de4
Daniel Puente <danpmpj@gmail.com>
2022-07-30 13:33:09 -0500

    Create variables.scss and config gulp watching for all scss files

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const sass = require("gulp-sass")(require("sass"));
 
 function css(done) {
     
-    src("src/scss/app.scss")// indetify file sass
+    src("src/scss/**/*.scss")// indetify file sass
         .pipe( sass())//compile
         .pipe(dest("build/css")); //Save in store
     done();
 }
 
 function dev(done){
-    watch("src/scss/app.scss", css)
+    watch("src/scss/**/*.scss", css)
 
     done();
 }
