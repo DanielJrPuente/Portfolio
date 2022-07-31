1464b1874948cd8799329f81c6bcb026e7abd0ad
Daniel Puente <danpmpj@gmail.com>
2022-07-31 18:02:12 -0500

    Make the header section add an image h1 and content

--- js/app.js
+++ js/app.js
@@ -12,13 +12,19 @@ function PageTransitions(){
     for(let i = 0; i < secBtn.length; i++){
         
         secBtn[i].addEventListener("click", function() {
+            //select the current button
             let currentBtn = document.querySelectorAll('.active-btn');
+            //remove active-btn class from the current button
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            //Add active-btn class from new button
             this.className += 'active-btn';
             
+            //select the current section
             let currentSec = document.querySelectorAll('.active');
+            //remove active class from the current section
             currentSec[0].className = currentSec[0].className.replace('active', '');
-            sections[i].classList.add('active');
+            //Add active class from new section
+            sections[i].classList.add('active'); 
         })
         //console.log(secBtn[i]);
     }
